f4987aade35666f3c425ebcbaae8635a0d1ccd2d
jessie5557 <ychen38@chicagobooth.edu>
2025-02-03 00:40:16 +0000

    submit

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get("/") do
 end
 
 
-get("/dynamic/:number_of_dice/:how_many_sides") do
+get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
   @sides = params.fetch("how_many_sides").to_i
 
